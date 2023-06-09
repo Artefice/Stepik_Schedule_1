2d1f6b7de5b3e9216bb32c8aec11b047a22ada29
Artefice <62757737+Artefice@users.noreply.github.com>
2023-06-09 13:07:48 +0700

    Add files via upload

---
+++ Stepik Schedule Course 1/src/main/java/com/example/stepik_schedule_1/StepikScheduleCourse1Application.java
@@ -0,0 +1,13 @@
+package com.example.stepik_schedule_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepikScheduleCourse1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StepikScheduleCourse1Application.class, args);
+    }
+
+}
---
+++ Stepik Schedule Course 1/src/test/java/com/example/stepik_schedule_1/StepikScheduleCourse1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stepik_schedule_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepikScheduleCourse1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
